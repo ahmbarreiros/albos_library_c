97a6d0d3fe819a06751a8d10174b5d7260850255
Antonio <ahmbarreiros.cs@gmail.com>
2023-09-16 16:12:04 -0300

    documentation for basic operations made, using doxygen syntax

--- albos_math.c
+++ albos_math.c
@@ -5,36 +5,129 @@
 #define INF_FLOAT (1 << (sizeof(float))) - 1
 #define INF (1L <<(sizeof(int))) - 1
 
+ // THIS IS A TEMPLATE FOR DOCUMENTING FUNCTIONS
+/**
+ * @brief Brief description of the function.
+ *
+ * Detailed description of the function's purpose, behavior, and usage.
+ * Include information about parameters, return values, and any exceptions.
+ *
+ * @param  (type) param1: Description of parameter 1.
+ * @param  (type) param2: Description of parameter 2.
+ * @return (type) Description of the return value (if applicable).
+ *
+ * @note Any additional notes or special considerations.
+ * @warning Any warnings or potential issues.
+ * @see Related functions or references.
+ */
+
+
+/** BASIC OPERATIONS */
+
+/**
+ * @brief Return the absolute value of a integer number.
+ *
+ * Given a integer value "x", returns the absolute value of the number.
+ * It accepts and returns long numbers.
+ *
+ * @param  (long) x: Long integer.
+ * @return (long) Integer "x" that represents the absolute value of parameter "x".
+ *
+ * @warning This function is for integer values only; for floats, check reference.
+ * @see abs_f() for floating point values.
+ */
 long abs_i(long x) {
+  
   if(x >= 0) return x;
   else return -x; 
 }
 
+
+/**
+ * @brief Return the absolute value of a floating point number.
+ *
+ * Given a floating point value "x", returns the absolute value of the number.
+ * It accepts and returns double numbers.
+ *
+ * @param  (double) x: Floating point value.
+ * @return (double) Double value "x" that represents the absolute value of parameter "x".
+ *
+ * @warning This function is for floating values only; for integers, check reference.
+ * @see abs_i() for integer values.
+ */
 double abs_f(double x) {
   if(x >= 0) return x;
   else return -x;
 }
 
+
+/**
+ * @brief Given two integer values, returns the biggest.
+ *
+ * @param  (int) x: Integer value.
+ * @param  (int) y: Integer value.
+ * @return (int) Compares parameters "x" and "y", and returns the biggest value between them.
+ *
+ * @warning This function is for integer values only; for floats, check reference.
+ * @see min_i() for smallest comparison.
+ */
 int max_i(int x, int y) {
   if(x > y) return x;
   else return y;
 }
 
+
+/**
+ * @brief Given two integer values, returns the smallest.
+ *
+ * @param  (int) x: Integer value.
+ * @param  (int) y: Integer value.
+ * @return (int) Compares parameters "x" and "y", and returns the smallest value between them.
+ *
+ * @warning This function is for integer values only; for floats, check reference.
+ * @see max_i() for biggest comparison.
+ */
 int min_i(int x, int y) {
   if(x <= y) return x;
   else return y;
 }
 
-float max_arr_f(float* values, int size) { 
+
+/**
+ * @brief Calculates the maximum value of a float array.
+ *
+ * Given an array of floating point values, calculates the biggest value within it.
+ *
+ * @param  (float*) arr : Array of floating point values.
+ * @param  (int)    size: Size of the array.
+ * @return (float)  The biggest value of the array "arr".
+ *
+ * @warning This function is for floating point values only; for integers, check reference.
+ * @see max_arr_i() for integer values.
+ */
+float max_arr_f(float* arr, int size) { 
   float max = -INF_FLOAT;
   for(int i = 0; i < size; i++) {
-    if(values[i] > max) {
-      max = values[i];
+    if(arr[i] > max) {
+      max = arr[i];
     }
   }
   return max;
 }
 
+
+/**
+ * @brief Calculates the maximum value of an integer array.
+ *
+ * Given an array of integer values, calculates the biggest value within it.
+ *
+ * @param  (int*) arr :  Array of integer values.
+ * @param  (int)  size:  Size of the array.
+ * @return (int)  The biggest value of the array "arr".
+ *
+ * @warning This function is for floating point values only; for integers, check reference.
+ * @see max_arr_i() for integer values.
+ */
 int max_arr_i(int* values, int size) {  
   int max = -INF;
   for(int i = 0; i < size; i++) {
@@ -45,26 +138,21 @@ int max_arr_i(int* values, int size) {
   return max;
 }
 
-double pow_f(float base, int exponent) {  
-  int isNegative = 0;
-  double raised = 1;
-  if(exponent == 0) return 1;
-  if(exponent < 0) {
-   isNegative = 1;
-   exponent = abs_i(exponent);
-  }
-  if(exponent == 1) return base;
-  else if(exponent == 2) return (base*base);
-  for(int i = 0; i < exponent; i++) {
-    raised *= base;
-  }
-  if(isNegative) {
-    return (1 / raised);
-  } else {
-    return raised;
-  }
-}
 
+/**
+ * @brief Calculates the power of an integer value.
+ *
+ * Given a number (base), calculates the multiplication of this number n (exponent) times.
+ * For example, 8 to the power 2 is equal to 8 * 8 = 64.
+ *
+ * @param  (int)  base    : Base number of the equation.
+ * @param  (int)  exponent: Exponent number of the equation.
+ * @return (long) The solution of the base to the power of the exponent.
+ *
+ * @note    This function does not accepts floating point exponents because it would involve nth root computation.
+ * @warning This function is for integer bases; for floating point bases, check reference.
+ * @see pow_f() for floating point bases.
+ */
 long pow_i(int base, int exponent) { 
   int isNegative = 0;
   long raised = 1;
@@ -85,18 +173,79 @@ long pow_i(int base, int exponent) {
   }
 }
 
-long sum_i(int* values, int size) {  
+
+/**
+ * @brief Calculates the power of a floating point value.
+ *
+ * Given a number (base), calculates the multiplication of this number n (exponent) times.
+ * For example, 8.0 to the power 2 is equal to 8.0 * 8.0 = 64.0.
+ *
+ * @param  (float)  base    : Base number of the equation.
+ * @param  (int)    exponent: Exponent number of the equation.
+ * @return (double) The solution of the base to the power of the exponent.
+ *
+ * @note    This function does not accepts floating point exponents because it would involve nth root computation.
+ * @warning This function is for floating point bases; for integer bases, check reference.
+ * @see pow_i() for integer bases.
+ */
+double pow_f(float base, int exponent) {  
+  int isNegative = 0;
+  double raised = 1;
+  if(exponent == 0) return 1;
+  if(exponent < 0) {
+   isNegative = 1;
+   exponent = abs_i(exponent);
+  }
+  if(exponent == 1) return base;
+  else if(exponent == 2) return (base*base);
+  for(int i = 0; i < exponent; i++) {
+    raised *= base;
+  }
+  if(isNegative) {
+    return (1 / raised);
+  } else {
+    return raised;
+  }
+}
+
+
+/**
+ * @brief Calculates the sum of the elements of an array.
+ *
+ * Given an array of integer values, calculates the sum of all elements.
+ *
+ * @param  (int*) arr : Array of integers.
+ * @param  (int)  size: Size of the array.
+ * @return (long) The sum of elements of the array "arr".
+ *
+ * @warning This function is for arrays of integer values; for arrays of floating point values, check reference
+ * @see sum_f() for arrays of floating point values.
+ */
+long sum_i(int* arr, int size) {  
   long sum = 0;
   for(int i = 0; i < size; i++) {
-    sum += values[i];
+    sum += arr[i];
   }
   return sum;
 }
 
-double sum_f(float* values, int size) {  
+
+/**
+ * @brief Calculates the sum of the elements of an array.
+ *
+ * Given an array of floating point values, calculates the sum of all elements.
+ *
+ * @param  (int*) arr : Array of floats.
+ * @param  (int)  size: Size of the array.
+ * @return (long) The sum of elements of the array "arr".
+ *
+ * @warning This function is for arrays of float values; for arrays of integers, check reference
+ * @see sum_i() for arrays of integer values.
+ */
+double sum_f(float* arr, int size) {  
   double sum = 0.0;
   for(int i = 0; i < size; i++) {
-    sum += values[i];
+    sum += arr[i];
   }
   return sum;
 }
@@ -104,7 +253,8 @@ double sum_f(float* values, int size) {
 
 
 
-/* ERROR CALCULATIONS */
+
+/** ERROR CALCULATIONS */
 
 float absolute_error(float value, float approximateValue) {
   return abs_f(value - approximateValue);
@@ -420,18 +570,33 @@ void gauss_resolution(float* mat, int sizeRows, int sizeColumns) {
   }
   for(k = 0; k < sizeRows; k++) {
     for(i = k + 1; i < sizeColumns; i++) {
-      m = mat[i * (sizeColumns) + k] / mat[k*(sizeColumns) + k];
+      if(mat[(k*(sizeColumns)) + k] == 0) {
+	printf("1\n");
+	m = 1;
+      } else {
+	m = mat[(i*(sizeColumns)) + k] / mat[(k*(sizeColumns)) + k];
+      }
       for(j = k; j < sizeColumns+1; j++) {
-	mat[i*(sizeColumns) + j] = mat[i*(sizeColumns) + j] - (m*mat[k*(sizeColumns) + j]);
+	mat[(i*(sizeColumns)) + j] = mat[(i*(sizeColumns)) + j] - (m*mat[(k*(sizeColumns)) + j]);
       }
     }
-    x[sizeRows-1] = mat[sizeRows*sizeRows + sizeRows-1] / mat[sizeRows*sizeRows + sizeRows-2];
+    if(mat[(sizeRows*sizeRows) + (sizeRows - 2)] == 0) {
+      printf("2\n");
+      x[sizeRows-1] = mat[(sizeRows*sizeRows) + sizeRows-1];
+    } else {
+      x[sizeRows-1] = mat[(sizeRows*sizeRows) + sizeRows-1] / mat[(sizeRows*sizeRows) + (sizeRows - 2)];
+    }
     for(i = sizeRows-2; i >= 0; i--) {
       s = 0;
       for(j = i+1; j < sizeRows; j++) {
 	s = s + mat[i*(sizeColumns) + j] * x[j];
       }
-      x[i] = (mat[i*(sizeColumns) + sizeRows] - s) / mat[i*(sizeColumns) + i];
+      if(mat[(i*sizeColumns) + i] == 0) {
+	printf("3\n");
+	x[i] = (mat[i*(sizeColumns) + sizeRows] - s);
+      } else {
+	x[i] = (mat[i*(sizeColumns) + sizeRows] - s) / mat[(i*sizeColumns) + i];
+      }
     }
   }
 
--- main.c
+++ main.c
@@ -2,21 +2,20 @@
 #include <stdio.h>
 
 int main() {
-  int intVector[3] = {-2, 1, 6};
+  
   //float matrix[3][4] = { {25555, 2401, 247, 10373.69}, {2401, 247, 31, 962.73}, {247, 31, 7, 97.76} };
-  int sizeRows = 4, sizeColumns = 5;
-  float matrix[4][5] = { {3, 2, 1, 2, 141},
-					  {6, 7, 4, 5, 382},
-					  {3, 8, 6, 7, 403},
-					  {9, 9, 7, 15, 677} };
-  //float matrix[3][4] = { {3, -0.1, -0.2, 7.85}, {0.1, 7, -0.3, -19.3}, {0.3, -0.2, 10, 71.4} };
+  
+  //TODO: error, possible division by 0
+  /* float matrix[3][4] = {{0, 8, 2, -7}, {3, 5, 2, 8}, {6, 2, 8, 26}}; */
+  
+  float matrix[2][3] = {{0.0003, 3, 2.0001}, {1, 1, 1}};
+  int sizeRows = 3, sizeColumns = 4;
+  
+  /* float matrix[3][4] = { {3, -0.1, -0.2, 7.85}, */
+  /* 			 {0.1, 7, -0.3, -19.3}, */
+  /* 			 {0.3, -0.2, 10, 71.4} }; */
 
   //float *res = gauss_elimination(*matrix, 3, 4);
   gauss_resolution(*matrix, sizeRows, sizeColumns); 
-  //float floatVector[3] = {1.2001, 0.99991, 0.92538};
-  //float result = maximum_norm_f(floatVector, 3);
-  //float resultFloat =  euclidean_norm_f(floatVector, 3);
-  //  printf("int: %d\nfloat: %f", resultInt, resultFloat);
-  //printf("result: %f\n", result);
  return 0;
 }
